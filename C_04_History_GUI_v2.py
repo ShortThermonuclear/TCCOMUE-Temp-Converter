a8a4c16db0702e7e47b2dee9772ced4c0341d18f
ShortThermonuclear <dediwalagej50493@masseyhigh.school.nz>
2026-02-26 11:49:22 +1300

    Working History

--- C_04_History_GUI_v2.py
+++ C_04_History_GUI_v2.py
@@ -1,6 +1,7 @@
 from tkinter import *
-from functools import partial # To prevent unwanted windows
+from functools import partial  # To prevent unwanted windows
 import all_constants as c
+
 
 class Converter:
     """
@@ -12,28 +13,31 @@ class Converter:
         Temperature converter GUI
         """
 
-        self.all_calculations_list = ['10.0°F is -12 °C', '20.0°F is -7 °C',
-                                      '30.0°F is -1 °C', '40.0°F is 4 °C',
-                                      '50.0°F is 10 °C', '60.0°F is 16 °C']
+        #
+        # self.all_calculations_list = ['10.0 °F is -12 °C', '20.0 °F is -7 °C',
+        #                               '30.0 °F is -1 °C', '40.0 °F is 4 °C',
+        #                               '50.0 °F is 10 °C', '60.0 °F is 16 °C']
+
+        self.all_calculations_list = ['10.0 °F is -12 °C', '20.0 °F is -7 °C',
+                                      '30.0 °F is -1 °C', '40.0 °F is 4 °C',
+                                      '50.0 °F is 10 °C']
 
         self.temp_frame = Frame(padx=10, pady=10)
         self.temp_frame.grid()
 
         self.to_history_button = Button(self.temp_frame,
-                                     text="History / Export",
-                                     bg="#CC6600",
-                                     fg="#FFFFFF",
-                                     font=["Arial", "14", "bold"], width=12,
-                                     command=self.to_history)
+                                        text="History / Export",
+                                        bg="#CC6600",
+                                        fg="#FFFFFF",
+                                        font=["Arial", "14", "bold"], width=12,
+                                        command=self.to_history)
         self.to_history_button.grid(row=1, padx=5, pady=5)
-
 
     def to_history(self):
         """
-        Opens history box and disables history button
-        (so users cannot create multiple windows)
+        Opens history dialogue box and disables history button
+        (so that users can't create multiple history boxes).
         """
-
         HistoryExport(self, self.all_calculations_list)
 
 
@@ -41,17 +45,17 @@ class HistoryExport:
     """
     Displays history dialogue box
     """
+
     def __init__(self, partner, calculations):
 
-        # setup dialogue box
         self.history_box = Toplevel()
 
-        # disable help button
+        # disables history button
         partner.to_history_button.config(state=DISABLED)
 
-        # If users press cross at top, closes help and
-        # 'releases' help button
-        self.history_box.protocol('WM_DELETE_WINDOW', partial(self.close_history, partner))
+        # Allows user to close the window using the cross at the top
+        self.history_box.protocol('WM_DELETE_WINDOW',
+                                  partial(self.close_history, partner))
 
         self.history_frame = Frame(self.history_box)
         self.history_frame.grid()
@@ -60,96 +64,88 @@ class HistoryExport:
         if len(calculations) <= c.MAX_CALCS:
             calc_back = "#D5E8D4"
             calc_amount = "all your"
-
         else:
             calc_back = "#ffe6cc"
             calc_amount = (f"your recent calculations - "
                            f"showing {c.MAX_CALCS} / {len(calculations)}")
 
-        # strings for 'long' labels
+        # strings for 'long' labels...
         recent_intro_txt = (f"Below are {calc_amount} calculations "
-                            f"(to the nearest degree)")
+                            "(to the nearest degree).")
 
         # Create string from calculations list (newest calculations first)
         newest_first_string = ""
         newest_first_list = list(reversed(calculations))
 
-        print("got to newest list and reversed it")
-        print("newest list", newest_first_list)
+        # Last item added in outside the for loop so that the spacing is correct
+        if len(newest_first_list) <= c.MAX_CALCS:
 
-        if len(newest_first_string) <= c.MAX_CALCS:
-
-            for item in newest_first_string[:-1]:
+            for item in newest_first_list[:-1]:
                 newest_first_string += item + "\n"
-                print("calc string so far...", newest_first_list)
 
             newest_first_string += newest_first_list[-1]
 
         # If we have more than five items...
         else:
-            for item in newest_first_string[:c.MAX_CALCS-1]:
+            for item in newest_first_list[:c.MAX_CALCS - 1]:
                 newest_first_string += item + "\n"
-                print("calc string so far...", newest_first_list)
 
-            newest_first_string += newest_first_list[c.MAX_CALCS-1]
+            newest_first_string += newest_first_list[c.MAX_CALCS - 1]
 
+        export_instruction_txt = ("Please push <Export> to save your calculations in a text "
+                                  "file.  If the filename already exists, it will be overwritten!")
 
-        export_instruction_txt = ("Please push <Export> to save your calculations in "
-                                  "file. If the the filename already exists, it will be ?replaced? ")
-
-        calculation = ""
-
-        # Label list (label text | Format | bg )
+        # Label list (label text | format | bg)
         history_labels_list = [
-            ["History / Export", "Arial", 16, "bold", None],
-            [recent_intro_txt, "Arial", 11,"", None],
-            [newest_first_string, "Arial", 14,"", calc_back],
-            [export_instruction_txt, "Arial", 11,"", None]
+            ["History / Export", ("Arial", "16", "bold"), None],
+            [recent_intro_txt, ("Arial", "11"), None],
+            [newest_first_string, ("Arial", "14"), calc_back],
+            [export_instruction_txt, ("Arial", "11"), None]
         ]
 
         history_label_ref = []
         for count, item in enumerate(history_labels_list):
-            make_label = Label(self.history_box, text=item[0], font=(item[1], item[2],item[3]),
-                               bg=item[4],
-                               wraplength=300, justify="left", padx=20, pady=10)
+            make_label = Label(self.history_box, text=item[0], font=item[1],
+                               bg=item[2],
+                               wraplength=300, justify="left", pady=10, padx=20)
             make_label.grid(row=count)
 
             history_label_ref.append(make_label)
 
-        # Retrieve export instructions label so that we can
-        # configure it to shw the filename if the use exports the file
+        # retrieve export instruction label so that we can
+        # configure it to show the filename if the user exports the file
         self.export_filename_label = history_label_ref[3]
 
         # make frame to hold buttons (two columns)
-        self.history_button_frame = Frame(self.history_box)
-        self.history_button_frame.grid(row = 4)
+        self.hist_button_frame = Frame(self.history_box)
+        self.hist_button_frame.grid(row=4)
 
-        buttons_ref_list = []
+        button_ref_list = []
 
-        # button list (button text | bg colout | command | row | column)
+        # button list (button text | bg colour | command | row | column)
         button_details_list = [
             ["Export", "#004C99", "", 0, 0],
-            ["Close", "#666666", partial(self.close_history, partner), 0 , 1]
+            ["Close", "#666666", partial(self.close_history, partner), 0, 1],
         ]
 
         for btn in button_details_list:
-            self.make_button = Button(self.history_button_frame,
-                                      font= ["Arial", "12", "bold"],
-                                      text = btn[0], bg = btn[1],
-                                      fg = "#FFFFFF", width = 12,
-                                      command = btn[2])
-            self.make_button.grid(row=btn[3], column = btn[4], padx= 10, pady=10)
+            self.make_button = Button(self.hist_button_frame,
+                                      font=["Arial", "12", "bold"],
+                                      text=btn[0], bg=btn[1],
+                                      fg="#FFFFFF", width=12,
+                                      command=btn[2])
+            self.make_button.grid(row=btn[3], column=btn[4], padx=10, pady=10)
 
     def close_history(self, partner):
         """
         Closes history dialogue box (and enables history button)
         """
-        # Put history button back to normal
+        # history button back to normal
         partner.to_history_button.config(state=NORMAL)
         self.history_box.destroy()
 
-        
-# Main routine
+
+# main routine
 if __name__ == "__main__":
     root = Tk()
     root.title("Temperature Converter")
